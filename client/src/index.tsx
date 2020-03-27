7fe08c73c1e03d07654c6a9807db41bba8416c1f
Anya <anya.s.litvinova@gmail.com>
2020-03-27 13:20:33 -0400

    updated data with mutations

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -14,13 +14,19 @@ const cache = new InMemoryCache();
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link: new HttpLink({
-    uri: "http://localhost:4000/",
+    uri: "http://localhost:4000/graphql",
+
     headers: {
-      authorization: localStorage.getItem("token"),
-      "client-name": "Space Explorer [web]",
-      "client-version": "1.0.0"
+      authorization: localStorage.getItem("token")
     }
   })
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem("token"),
+    cartItems: []
+  }
 });
 
 
